49079556f57fbcd9ef0c3cf90214ce6f56d2743b
赵依静 <zhaoyijing@coocaa.com>
2021-05-11 17:02:36 +0800

    refactor(Package): 修改name|添加ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,14 @@ module.exports = {
   base: '/online-clip/',
   title: 'Online Clip',
   description: 'A video editor',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
     search: false,
     nav: [
       { text: '使用指南', link: '' },
-      { text: '在线体验', link: 'http://172.20.155.102/online-clip-editor/' },
+      { text: '在线体验', link: 'http://172.20.155.102/online-clip-editor' },
       { text: 'GitLab', link: 'http://gitlab.skysri.com/flower/online-clip.git' }
     ]
   }
