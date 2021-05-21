25a4745d4ca381ca0a71e224f2ae14d4a0ab6a01
赵依静 <zhaoyijing@coocaa.com>
2021-05-21 18:16:15 +0800

    feat(README): 新增使用指南

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,9 +8,15 @@ module.exports = {
   themeConfig: {
     search: false,
     nav: [
-      { text: '使用指南', link: '' },
+      { text: '使用指南', link: '/guide/' },
       { text: '在线体验', link: 'http://172.20.155.102/online-clip-editor' },
       { text: 'GitLab', link: 'http://gitlab.skysri.com/flower/online-clip.git' }
+    ],
+    sidebar: [
+      {
+        title: '使用指南',
+        path: '/guide/',
+      }
     ]
   }
 }
