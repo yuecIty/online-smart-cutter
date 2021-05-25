c003053fe6902b129dd984018df1882162a80721
赵依静 <zhaoyijing@coocaa.com>
2021-05-25 17:06:29 +0800

    fix(VueConfigJs): 修复PublicPath问题

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-const ENV = process.env.NODE_ENV
-const PORT = process.env.PORT || '8003'
-const HOST = process.env.HOST || '127.0.0.1'
-const baseUrl = ENV === 'production' ? '' : `http://${HOST}:${PORT}`
 module.exports = {
-  publicPath: baseUrl
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/online-clip-editor/'
+    : '/'
 }
